ba9d5f7632723a331eb234e7342cdc24a1c36b24
Dolna Sornali <93023509+dolna1610@users.noreply.github.com>
2022-10-24 15:15:25 +0600

    Add files via upload

---
+++ src/main/java/java_condition_loop_problem_solution/LargestSmallestNumbers.java
@@ -0,0 +1,49 @@
+package conditional_statement_loop;
+//10. Write a program to enter the numbers till the user wants and at the end, the program should display the largest and smallest numbers user entered.
+//Input:
+//23
+//54
+//12
+//21
+//67
+//98
+//q
+//Output:
+//The largest number is: 98
+//The smallest number is: 12
+
+import java.util.Scanner;
+public class LargestSmallestNumbers {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            //saving first input as min and max
+            System.out.println("Enter the first number:");
+            int number=sc.nextInt();
+            int max=number;
+            int min=number;
+            //looping till user hits "End"
+            while (true){
+                System.out.println("Give input as a number:\n" +
+                        "if stop Press \"End\"");
+                if(sc.hasNextInt()){
+                    int num= sc.nextInt();
+                    if (num>max){
+                        max=num;
+                    }
+                    else{
+                        min=num;
+                    }
+                }
+                else{
+                    String s=sc.next();
+                    System.out.println("Input printed "+s);
+                    if(s.equals("End")){
+                        break;
+                    }
+                }
+            }
+            System.out.println("The largest number is "+max);
+            System.out.println("The smallest number is "+min);
+
+        }
+    }
